6253e42dfc7d36e456156f3960b8bf3746ecbb5f
root <root@vps487790.ovh.net>
2018-02-08 22:03:20 +0100

    jenkins run-script

--- jenkins/run.sh
+++ jenkins/run.sh
@@ -5,7 +5,7 @@ docker run \
 	--rm \
 	-d \
 	-u root \
-	-p 8080:8080 \
+	-p 80:8080 \
 	-p 50000:50000 \
 	-v /var/run/docker.sock:/var/run/docker.sock \
 	-v jenkins_data:/var/jenkins_home atarax/jenkins
